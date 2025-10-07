2d04ae9e6dd38c607a7423857af66f071284897b
Yerkhan <231132@astanait.edu.kz>
2025-10-07 22:58:03 +0500

    one ideal blue sphere

--- CG_Project/CG_Project.cpp
+++ CG_Project/CG_Project.cpp
@@ -1,12 +1,14 @@
 ﻿#define _USE_MATH_DEFINES
+#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <fstream>
 #include <sstream>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 unsigned int compileShader(const char* vertexPath, const char* fragmentPath) {
     std::ifstream vShaderFile(vertexPath);
@@ -19,55 +21,46 @@ unsigned int compileShader(const char* vertexPath, const char* fragmentPath) {
     const char* vShaderSource = vertexCode.c_str();
     const char* fShaderSource = fragmentCode.c_str();
 
-    unsigned int vertex, fragment;
-    vertex = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex, 1, &vShaderSource, NULL);
     glCompileShader(vertex);
 
-    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment, 1, &fShaderSource, NULL);
     glCompileShader(fragment);
 
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertex);
-    glAttachShader(shaderProgram, fragment);
-    glLinkProgram(shaderProgram);
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
 
     glDeleteShader(vertex);
     glDeleteShader(fragment);
 
-    return shaderProgram;
+    return program;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
 void generateSphere(std::vector<float>& vertices, std::vector<unsigned int>& indices, int sectorCount, int stackCount) {
-    float x, y, z, xy;
-    float nx, ny, nz, lengthInv = 1.0f;
-    float sectorStep = 2 * M_PI / sectorCount;
-    float stackStep = M_PI / stackCount;
-    float sectorAngle, stackAngle;
     float radius = 0.6f;
-
+    float x, y, z, xy;
+    float nx, ny, nz;
+    float sectorStep = 2.0f * (float)M_PI / sectorCount;
+    float stackStep = (float)M_PI / stackCount;
     for (int i = 0; i <= stackCount; ++i) {
-        stackAngle = M_PI / 2 - i * stackStep;
+        float stackAngle = (float)M_PI / 2 - i * stackStep;
         xy = radius * cosf(stackAngle);
         z = radius * sinf(stackAngle);
-
         for (int j = 0; j <= sectorCount; ++j) {
-            sectorAngle = j * sectorStep;
+            float sectorAngle = j * sectorStep;
             x = xy * cosf(sectorAngle);
             y = xy * sinf(sectorAngle);
-            nx = x * lengthInv;
-            ny = y * lengthInv;
-            nz = z * lengthInv;
+            nx = x;
+            ny = y;
+            nz = z;
             vertices.push_back(x);
             vertices.push_back(y);
             vertices.push_back(z);
@@ -77,10 +70,9 @@ void generateSphere(std::vector<float>& vertices, std::vector<unsigned int>& ind
         }
     }
 
-    int k1, k2;
     for (int i = 0; i < stackCount; ++i) {
-        k1 = i * (sectorCount + 1);
-        k2 = k1 + sectorCount + 1;
+        int k1 = i * (sectorCount + 1);
+        int k2 = k1 + sectorCount + 1;
         for (int j = 0; j < sectorCount; ++j, ++k1, ++k2) {
             if (i != 0) {
                 indices.push_back(k1);
@@ -96,34 +88,80 @@ void generateSphere(std::vector<float>& vertices, std::vector<unsigned int>& ind
     }
 }
 
+void perspective(float* m, float fovy, float aspect, float near, float far) {
+    float f = 1.0f / tanf(fovy / 2.0f);
+    for (int i = 0; i < 16; ++i) m[i] = 0.0f;
+    m[0] = f / aspect;
+    m[5] = f;
+    m[10] = (far + near) / (near - far);
+    m[11] = -1.0f;
+    m[14] = (2.0f * far * near) / (near - far);
+}
+
+void translate(float* m, float x, float y, float z) {
+    for (int i = 0; i < 16; ++i) m[i] = 0.0f;
+    m[0] = 1.0f;
+    m[5] = 1.0f;
+    m[10] = 1.0f;
+    m[15] = 1.0f;
+    m[12] = x;
+    m[13] = y;
+    m[14] = z;
+}
+
+void rotateY(float* m, float angle) {
+    float c = cosf(angle);
+    float s = sinf(angle);
+    for (int i = 0; i < 16; ++i) m[i] = 0.0f;
+    m[0] = c;
+    m[1] = 0.0f;
+    m[2] = -s;
+    m[3] = 0.0f;
+    m[4] = 0.0f;
+    m[5] = 1.0f;
+    m[6] = 0.0f;
+    m[7] = 0.0f;
+    m[8] = s;
+    m[9] = 0.0f;
+    m[10] = c;
+    m[11] = 0.0f;
+    m[12] = 0.0f;
+    m[13] = 0.0f;
+    m[14] = 0.0f;
+    m[15] = 1.0f;
+}
+
 int main() {
-    glfwInit();
+    if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Spheres Visualization", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Sphere Visualization", NULL, NULL);
-    if (!window) return -1;
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwTerminate();
+        return -1;
+    }
 
     std::vector<float> vertices;
     std::vector<unsigned int> indices;
-    generateSphere(vertices, indices, 36, 18);
+    generateSphere(vertices, indices, 48, 24);
 
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -133,18 +171,46 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window)) {
-        processInput(window);
+        float time = (float)glfwGetTime();
+        float angle = time * 0.7f;
+
+        float model[16];
+        rotateY(model, angle);
+
+        float view[16];
+        translate(view, 0.0f, 0.0f, -3.0f);
+
+        float projection[16];
+        perspective(projection, 45.0f * (float)M_PI / 180.0f, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
         glClearColor(0.05f, 0.05f, 0.08f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+        int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        int projLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, model);
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, view);
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, projection);
+
+        glUniform3f(glGetUniformLocation(shaderProgram, "lightPos"), 1.2f, 1.0f, 2.0f);
+        glUniform3f(glGetUniformLocation(shaderProgram, "viewPos"), 0.0f, 0.0f, 3.0f);
+        glUniform3f(glGetUniformLocation(shaderProgram, "lightColor"), 1.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(shaderProgram, "objectColor"), 0.4f, 0.6f, 0.9f);
+
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, (GLsizei)indices.size(), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
--- CG_Project/shader.vert
+++ CG_Project/shader.vert
@@ -2,7 +2,12 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 out vec3 Normal;
+out vec3 FragPos;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 void main() {
-    gl_Position = vec4(aPos, 1.0);
-    Normal = aNormal;
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(model) * aNormal;
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }
